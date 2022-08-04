93a6918db155527fcc4d417f668c885ba2bc853e
Gustavo Marques <gustavo99a@hotmail.com>
2022-08-04 11:42:12 -0300

    o projeto ainda não está completo, falta alguns detalhes na aba de projetos e deixar o mesmo responsivo, as alterações virão em futuros commits

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,28 +1,42 @@
+const writeProjects = document.getElementById('writeProjects');
 const botaoDark = document.getElementById('botao-dark');
 const body = document.getElementsByTagName('body')[0];
-const darkModeClass = 'dark-mode'; 
+const showButton = document.getElementById('show-btn')
+const hideButton = document.getElementById('hide-btn')
+const darkModeClass = 'dark-mode';
 
-botaoDark.addEventListener('click', changeMode);
 
-function changeMode(){
-    changeBody();
-    changeButton();
+botaoDark.addEventListener('click', changeBody);
+showButton.addEventListener('click', showText);
+hideButton.addEventListener('click', hideText);
+
+function hideText(){
+    if(writeProjects.style.display === "flex"){
+        writeProjects.style.display = "none"
+    }
+}
+
+function showText(){
+    writeProjects.style.display = "none";
+    
+    if (writeProjects.style.display === "none"){
+        writeProjects.style.display = "flex";
+        writeProjects.style.flexDirection = "column";
+    }
 }
 
 function changeBody() {
     body.classList.toggle(darkModeClass);
-  
 }
 
-function changeButton(){
-    const darkMode = "Dark-Mode";
-    const purpleMode = "Purple-Mode";
+/*function changeButton(){
+    const darkMode = "dark-mode";
+    const purpleMode = "puple-mode";
 
     if(body.classList.contains(darkModeClass)){
-        botaoDark.innerHTML = purpleMode;
-        
+        botaoDark.innerHTML = purpleMode; 
     }
     else{
         botaoDark.innerHTML = darkMode;
     }
-}
+     está função serve para alterar o texto, caso o botão do dark mode sejam diferentes, mas como só uso uma imagem, não precisa*/
